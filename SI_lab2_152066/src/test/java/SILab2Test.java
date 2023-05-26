47ee1626dea7870945ab889e5cf9eeb155e83a32
miceskidejan <dejan.miceski@students.finki.ukim.mk>
2023-05-26 12:41:03 +0200

    Add tests

--- SI_lab2_152066/src/test/java/SILab2Test.java
+++ SI_lab2_152066/src/test/java/SILab2Test.java
@@ -9,50 +9,72 @@ import static org.junit.jupiter.api.Assertions.*;
 class SILab2Test {
 
     @Test
-    void userIsNull() {
+    void everyBranch() {
+        //1
         User user = null;
         List<User> allUsers = new ArrayList<>();
 
         assertThrows(RuntimeException.class, ()->{
             SILab2.function(user,allUsers);
         });
+
+        //2
+        User user1 = new User(null,"Test1234!","test@test.test");
+        User user2 = new User("test@test.test","Test1234!","test@test.test");
+        User user3 = new User("test@test.test1","Test1234!","test@test.test1");
+        List<User> allUsers1 = Arrays.asList(user2,user3);
+
+        assertFalse(SILab2.function(user1,allUsers1));
+
+        //3
+        User user4 = new User("username","Test1!","test.test");
+        List<User> allUsers2 = Arrays.asList();
+
+        assertFalse(SILab2.function(user4,allUsers2));
+
+        //4
+        User user5 = new User("username","Test1234","test.test");
+        List<User> allUsers3 = Arrays.asList();
+
+        assertFalse(SILab2.function(user5,allUsers3));
+
+        //5
+        User user6 = new User("username","Test 1234","test.test");
+        List<User> allUsers4 = Arrays.asList();
+
+        assertFalse(SILab2.function(user6,allUsers4));
+
     }
 
     @Test
-    void sameIsZeroFalse(){
-        User user = new User(null,"Test1234!","test@test.test");
-        User user1 = new User("test@test.test","Test1234!","test@test.test");
-        User user2 = new User("test@test.test1","Test1234!","test@test.test1");
-        List<User> allUsers = Arrays.asList(user1,user2);
+    void multipleCondition(){
 
-        assertFalse(SILab2.function(user,allUsers));
+        List<User> allUsers = new ArrayList<>();
+
+        //1
+        User user = null;
+        assertThrows(RuntimeException.class, ()->{
+            SILab2.function(user,allUsers);
+        });
+
+        //2
+        User user1 = new User("username", null,null);
+
+        assertThrows(RuntimeException.class, ()->{
+            SILab2.function(user1,allUsers);
+        });
+
+        //3
+        User user2 = new User("username", "Test123!",null);
+
+        assertThrows(RuntimeException.class, ()->{
+            SILab2.function(user2,allUsers);
+        });
+
+        //4
+        User user3 = new User("username", "Test","test@test");
+
+        assertFalse(SILab2.function(user3,allUsers));
+
     }
-
-    @Test
-    void invalidPasswordShort(){
-        User user = new User("username","Test1!","test.test");
-        List<User> allUsers = Arrays.asList();
-
-        assertFalse(SILab2.function(user,allUsers));
-    }
-
-    @Test
-    void invalidPasswordNotContainsSpecialCharacter(){
-        User user = new User("username","Test1234","test.test");
-        List<User> allUsers = Arrays.asList();
-
-        assertFalse(SILab2.function(user,allUsers));
-    }
-
-    @Test
-    void invalidPasswordContainsEmptySpace(){
-        User user = new User("username","Test 1234","test.test");
-        List<User> allUsers = Arrays.asList();
-
-        assertFalse(SILab2.function(user,allUsers));
-    }
-
-
-
-
 }
